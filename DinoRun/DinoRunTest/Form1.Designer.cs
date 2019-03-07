742d9dac17a336e7c5dd8a8473f469420f0b42e1
acerjt <acersjt@gmail.com>
2019-03-08 00:11:16 +0700

    final

---
+++ DinoRun/DinoRunTest/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace DinoRunTest
+{
+    partial class screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(screen));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Label_Score = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.Cloud_3 = new System.Windows.Forms.PictureBox();
+            this.Cloud_1 = new System.Windows.Forms.PictureBox();
+            this.Cloud_2 = new System.Windows.Forms.PictureBox();
+            this.Cactus = new System.Windows.Forms.PictureBox();
+            this.LineRun = new System.Windows.Forms.PictureBox();
+            this.Trex = new System.Windows.Forms.PictureBox();
+            this.Label_HighScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Cloud_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cloud_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cloud_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cactus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LineRun)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Trex)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Label_Score
+            // 
+            this.Label_Score.Font = new System.Drawing.Font("Algerian", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_Score.Location = new System.Drawing.Point(1357, 9);
+            this.Label_Score.Name = "Label_Score";
+            this.Label_Score.Size = new System.Drawing.Size(106, 47);
+            this.Label_Score.TabIndex = 4;
+            this.Label_Score.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Cloud_3
+            // 
+            this.Cloud_3.Image = ((System.Drawing.Image)(resources.GetObject("Cloud_3.Image")));
+            this.Cloud_3.Location = new System.Drawing.Point(113, 229);
+            this.Cloud_3.Name = "Cloud_3";
+            this.Cloud_3.Size = new System.Drawing.Size(100, 50);
+            this.Cloud_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cloud_3.TabIndex = 7;
+            this.Cloud_3.TabStop = false;
+            // 
+            // Cloud_1
+            // 
+            this.Cloud_1.Image = ((System.Drawing.Image)(resources.GetObject("Cloud_1.Image")));
+            this.Cloud_1.Location = new System.Drawing.Point(549, 105);
+            this.Cloud_1.Name = "Cloud_1";
+            this.Cloud_1.Size = new System.Drawing.Size(100, 50);
+            this.Cloud_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cloud_1.TabIndex = 6;
+            this.Cloud_1.TabStop = false;
+            // 
+            // Cloud_2
+            // 
+            this.Cloud_2.Image = ((System.Drawing.Image)(resources.GetObject("Cloud_2.Image")));
+            this.Cloud_2.Location = new System.Drawing.Point(876, 322);
+            this.Cloud_2.Name = "Cloud_2";
+            this.Cloud_2.Size = new System.Drawing.Size(100, 50);
+            this.Cloud_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cloud_2.TabIndex = 5;
+            this.Cloud_2.TabStop = false;
+            // 
+            // Cactus
+            // 
+            this.Cactus.BackColor = System.Drawing.Color.Transparent;
+            this.Cactus.Image = ((System.Drawing.Image)(resources.GetObject("Cactus.Image")));
+            this.Cactus.Location = new System.Drawing.Point(-30, 523);
+            this.Cactus.Name = "Cactus";
+            this.Cactus.Size = new System.Drawing.Size(24, 85);
+            this.Cactus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Cactus.TabIndex = 2;
+            this.Cactus.TabStop = false;
+            // 
+            // LineRun
+            // 
+            this.LineRun.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.LineRun.Image = ((System.Drawing.Image)(resources.GetObject("LineRun.Image")));
+            this.LineRun.Location = new System.Drawing.Point(-4, 581);
+            this.LineRun.Name = "LineRun";
+            this.LineRun.Size = new System.Drawing.Size(2402, 28);
+            this.LineRun.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.LineRun.TabIndex = 1;
+            this.LineRun.TabStop = false;
+            // 
+            // Trex
+            // 
+            this.Trex.BackColor = System.Drawing.Color.Transparent;
+            this.Trex.Image = global::DinoRunTest.Properties.Resources.run;
+            this.Trex.Location = new System.Drawing.Point(18, 503);
+            this.Trex.Name = "Trex";
+            this.Trex.Size = new System.Drawing.Size(88, 94);
+            this.Trex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Trex.TabIndex = 8;
+            this.Trex.TabStop = false;
+            // 
+            // Label_HighScore
+            // 
+            this.Label_HighScore.Font = new System.Drawing.Font("Algerian", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label_HighScore.Location = new System.Drawing.Point(1118, 9);
+            this.Label_HighScore.Name = "Label_HighScore";
+            this.Label_HighScore.Size = new System.Drawing.Size(229, 47);
+            this.Label_HighScore.TabIndex = 9;
+            this.Label_HighScore.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // screen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1500, 647);
+            this.Controls.Add(this.Label_HighScore);
+            this.Controls.Add(this.Trex);
+            this.Controls.Add(this.Cloud_3);
+            this.Controls.Add(this.Cloud_1);
+            this.Controls.Add(this.Cloud_2);
+            this.Controls.Add(this.Label_Score);
+            this.Controls.Add(this.Cactus);
+            this.Controls.Add(this.LineRun);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "screen";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.Cloud_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cloud_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cloud_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cactus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LineRun)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Trex)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox LineRun;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Cactus;
+        private System.Windows.Forms.Label Label_Score;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox Cloud_2;
+        private System.Windows.Forms.PictureBox Cloud_1;
+        private System.Windows.Forms.PictureBox Cloud_3;
+        private System.Windows.Forms.PictureBox Trex;
+        private System.Windows.Forms.Label Label_HighScore;
+    }
+}
+
---
+++ DinoRun/DinoRunTest/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+namespace DinoRunTest
+{
+    public partial class screen : Form
+    {
+        #region
+        bool jump; //Biến luận lý khủng long nhảy
+        //bool godown; 
+        bool exit; //Biến luận lý thoát game
+        bool restart;//Biến luận lý bắt đầu lại game
+        //bool mute;
+        int score = 0;//Biến nguyên điểm số
+        int highscore;//Biến nguyên điểm cao
+        int speed = 9;//Biến nguyên tốc độ game
+        Random rand = new Random(); //Lấy ngẫu nhiên giá trị
+        int playerSpeed = 36; //Biến nguyên tốc độ khủng long
+        int gravity=3;//Biến nguyên trọng lực
+        int index;//Biến nguyên để lấy giá trị ngẫu nhiên
+        int[] b={250,600,850,1000,1300,2000,2600,3000,3300};//Mảng lưu điểm để tăng tốc độ
+        int[] a = { 150,300,450,600,750,900,1050,1200,1350,1500,1650,1800,1950,2100,
+                    2250,2400,2550,2700,2850,3000,3150,3300,3450,3600,3750,3900,4050,
+                    4200,4350,4500};//Mảng lưu điểm để phát âm khi đạt các mức điểm
+        int Force;//Mức nhảy
+        SoundPlayer jumpaudio = new SoundPlayer(DinoRunTest.Properties.Resources.jump);
+        SoundPlayer scoreaudio = new SoundPlayer(DinoRunTest.Properties.Resources.score);
+        SoundPlayer dieaudio = new SoundPlayer(DinoRunTest.Properties.Resources.die);
+        #endregion   //Khởi tạo các biến giá trị
+        public screen()
+        {
+            InitializeComponent(); 
+        }//Khởi tạo form
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        } //Load form (Những picturebox và label bên design)
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)                          //Nếu nhấn phím mũi tên lên 
+            if ((Trex.Top == LineRun.Top - Trex.Height + 10)) //và vị trí của khủng long đang ở mặt đất
+            {                                                //thì khủng long sẽ 
+                jump = true;                                //nhảy
+                    Force = playerSpeed;                   //một khoảng force=playerspeed
+                    jumpaudio.Play();                     //và đồng thời sẽ phát ra âm thanh nhảy  
+                }
+            //if (e.KeyCode == Keys.Down)
+            //{
+            //    godown = true;
+            //}
+            //if (e.KeyCode == Keys.M)
+            //{
+            //    mute = true;
+            //}
+            if (e.KeyCode == Keys.Space)
+            {
+                if ((Trex.Top == LineRun.Top - Trex.Height + 10)) //Nếu nhấn phím space
+                {                                                //và vị trí của khủng long đang ở mặt đất
+                    jump = true;                                //thì khủng long sẽ 
+                    Force = playerSpeed;                       //nhảy
+                    jumpaudio.Play();                         //một khoảng force=playerspeed
+                }                                            //và đồng thời sẽ phát ra âm thanh nhảy  
+            }
+            if (e.KeyCode == Keys.Enter)                         //Nếu nhấn phím Enter
+                if (Trex.Bounds.IntersectsWith(Cactus.Bounds))  //và khủng long đụng chướng ngại vật(Xương rồng)
+                {                                              //thì
+                    restart = true;                           //cho phép bắt đầu lại game
+                }
+         
+            if (e.KeyCode == Keys.Escape)//Nếu nhấn phím Escape
+                exit = true;            //Thoát chương trình
+        }//hàm bắt keyboard
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+          
+            if (e.KeyCode == Keys.Enter)
+                restart = false;
+            //if (e.KeyCode == Keys.Down)
+            //{
+
+            //    godown = false;
+            //}
+            if (e.KeyCode == Keys.Escape)
+                exit = false;
+        }
+        private void makeLineRun()
+        {
+            PictureBox LineRunNew = new PictureBox();//Khởi tạo một picturebox
+            LineRunNew.Image = LineRun.Image;       //Gán ảnh của picturebox này 
+            LineRunNew.Height = LineRun.Height;    //bằng ảnh của picturebox_linerun ở phần design
+            LineRunNew.Width = LineRun.Width;     //và cả kích thước cũng bằng 
+            LineRunNew.Left = 300;               //kích thước của picturebox_linerun ở phần design
+            LineRunNew.Top = LineRun.Top;       //vị trí nằm sau picturebox_linerun ở phần design
+            LineRunNew.Tag = "linerun";        //gán tag
+            this.Controls.Add(LineRunNew);    //và add vào control để quản lí trong quá trình game chạy  
+        }// Hàm tạo đường chạy của khủng long
+        private void changeCactus()
+        {
+           
+            index = rand.Next(1, 9); // Lấy ngẫu nhiên 1 số từ 1 đến 9
+            switch (index)
+            {
+                case 1:   
+                    Cactus.Top = 425;
+                    Cactus.Image = Properties.Resources.smallcatus_1;
+                    Cactus.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+                case 2:
+                    Cactus.Top = 425;
+                    Cactus.Image = Properties.Resources.smallcatus_2_1;
+                    Cactus.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+                case 3:
+                    Cactus.Top = 425;
+                    Cactus.Image = Properties.Resources.smallcatus_2;
+                    Cactus.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+                case 4:
+                    Cactus.Top = 425;
+                    Cactus.Image = Properties.Resources.smallcatus_3;
+                    Cactus.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+                case 5:
+                    Cactus.Top = 400;
+                    Cactus.Image = Properties.Resources.largecatus_1;
+                    Cactus.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+                case 6:
+                    Cactus.Top = 400;
+                    Cactus.Image = Properties.Resources.largecatus_2;
+                    Cactus.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+                case 7:
+                    Cactus.Top = 400;
+                    Cactus.Image = Properties.Resources.largecatus_2_1;
+                    Cactus.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+                case 8:
+                    Cactus.Top = 400;
+                    Cactus.Image = Properties.Resources.largecatus_3_1;
+                    Cactus.SizeMode = PictureBoxSizeMode.AutoSize;
+                    break;
+            }//Chọn ra một ảnh từ số lấy ngẫu nhiên
+        }//Hàm thay đổi cây xương rồng
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //if(mute==true)
+            //{
+            //    scoreaudio.Stop();
+            //    jumpaudio.Stop();
+            //    dieaudio.Stop();
+            //}
+            score += 1; //Tăng điểm khi bắt đầu game
+            LineRun.Left -= speed;//Picturebox di chuyển sang trái màn hình theo tốc độ speed tạo hiệu ứng chạy
+            foreach (int x in a)
+            {
+                if (x == score)
+                    scoreaudio.Play();
+            }//Sau mỗi móc điểm nhất định trong mảng a sẽ phát âm thanh ghi điểm
+            foreach (int x in b)
+            {
+                if (score == x)
+                {
+                    speed += 6;
+                    gravity += 3;
+                    playerSpeed += 15;
+                }
+            }//Sau mỗi móc điểm nhất định trong mảng b sẽ tăng tốc độ game
+            Label_Score.Text = " "+score;//Hiển thị điểm lên Label
+            Cactus.Left -= speed;//Xương rồng di chuyển sang trái theo tốc độ speed
+            Cloud_1.Left -= speed;//Mây di chuyển sang trái theo tốc độ speed
+            Cloud_2.Left -= speed;//Mây di chuyển sang trái theo tốc độ speed
+            Cloud_3.Left -= speed;//Mây di chuyển sang trái theo tốc độ speed
+            if (exit ==true)//Thoát chương trình
+                this.Close();
+            if (Cloud_1.Left < -150)//Đặt lại vị trí của mây khi chạy ra khỏi màn hình
+                Cloud_1.Left = 1100;               
+            if (Cloud_2.Left < -500)//Đặt lại vị trí của mây khi chạy ra khỏi màn hình
+                Cloud_2.Left = 2500;
+            if (Cloud_3.Left < -300)//Đặt lại vị trí của mây khi chạy ra khỏi màn hình
+                Cloud_3.Left = 1500;
+            if (Cactus.Left < -150)//Đặt lại vị trí của xương rồng khi chạy ra khỏi màn hình
+            {
+                Cactus.Left = rand.Next(1100, 1200);//Đặt lại vị trí của xương rồng trong khoảng (1100-1200)
+                changeCactus();//Thay đổi xương rồng
+            }
+            if (jump == true)//Nếu nhấn space
+            {
+                Trex.Top -= Force;//Khủng long nhảy lên một khoảng force
+                Force -= gravity;// Khủng long rơi xuống sau khi nhảy
+            }
+            if (Trex.Top + Trex.Height >= LineRun.Top || Trex.Top + Trex.Height < 100)
+            {//nếu vị trí của khủng long nhảy cao so với qui định hoặc chệch ra khỏi đường chạy
+                jump = false;
+                Trex.Top = LineRun.Top - Trex.Height + 10;//Đặt vị trí khủng long trên đường chạy
+                //jump = false;
+            }
+            else
+                Trex.Top += gravity;
+
+            //if (godown == true)
+            //{
+            //    Trex.Image = Properties.Resources.duck;
+            //    //Trex.Height = 52;
+            //    //Trex.Width = 101;
+            //    Trex.SizeMode = PictureBoxSizeMode.StretchImage;
+            //}
+            if (LineRun.Left < -66)   //Nếu picturebox_linerun chạy ra khỏi màn hình
+                makeLineRun();       //tạo một linerun đằng sau nó
+            if (LineRun.Left < -886)//Nếu picturebox_linerun mất khỏi màn hình
+                LineRun.Left = 886;//thì đặt nó sau màn hình
+
+            if (
+               Trex.Bounds.IntersectsWith(Cactus.Bounds)//Nếu Khủng Long va chạm xương rồng(GameOver)
+               )
+            {
+                
+                dieaudio.Play();//Phát âm thanh kết thúc
+                Trex.Image = Properties.Resources.die1;//Thay đổi hình picturebox
+                if (score >= highscore)//Nếu điểm hiện tại lớn hơn điểm highscore
+                {
+                    highscore = score;//Gán highscore bằng điểm hiện tại
+                    Label_HighScore.Text = "HI         " + highscore;//Hiển thị điểm đó lên Label_HighScore
+                }
+                timer1.Stop();//Dừng Timer1(Game Kết Thúc)
+
+                PictureBox Restart = new PictureBox();              //Khởi tạo picturebox Restart
+                Restart.Image = Properties.Resources.restart;      //Gán hình restart cho picturebox
+                Restart.Location = new Point(370, 240);           //Đặt picturebox ở giữa màn hình
+                Restart.SizeMode = PictureBoxSizeMode.AutoSize;  //Căn kích thước picturebox
+                Restart.Anchor = AnchorStyles.None;
+                Restart.Tag = "restart";                       //Gắn tag
+                this.Controls.Add(Restart);                   //Thêm vào control để điều khiển
+                timer2.Start();                              //Bắt đầu Timer2 (Khởi động lại game) 
+            }
+            foreach (Control X in this.Controls)
+            {
+               
+                if (X is PictureBox && X.Tag == "linerun")
+                {
+                    X.Left -=speed;//Di chuyển linerun sang trái màn hình tạo hiệu ứng chạy cho khủng long
+                    if (X.Left < -1772)//Nếu vị trí của linerun mất khỏi màn hình 
+                    {
+                        this.Controls.Remove(X);//thì xoá picturebox linerun đó
+                        X.Dispose();
+                    }
+                }
+                if (X is PictureBox && X.Tag == "restart" && restart==true)//Nếu nhấn enter 
+                    this.Controls.Remove(X);                              //xoá picturebox Restart
+            }
+        }//Bắt đầu Game
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (restart == true)//Nếu nhấn enter
+            {
+
+                Cactus.Left = 1000;//đặt lại vị trí của xương rồng
+                foreach (Control X in this.Controls)
+                    if (X is PictureBox && X.Tag == "restart" && restart == true)//Xoá pictruebox Restart
+                        this.Controls.Remove(X);                                //khi bắt đầu lại game
+                Trex.Image = Properties.Resources.run;                         //Đặt lại hình cho khủng long
+                speed = 9;                                                    //Đặt lại tốc độ
+                //Random rand = new Random();       
+                playerSpeed = 36;                                           //Đặt lại tốc độ khủng long
+                gravity = 3;                                               //Đặt lại trọng lực khủng long
+                timer1.Start();                                           //Bắt đầu lại game
+                score = 0;                                               //Đặt lại điểm về 0
+            }
+            restart = false;
+                if (exit == true)//Thoát chương trình khi nhấn escape
+                this.Close();
+        }//Bắt đầu lại game
+    }
+}
